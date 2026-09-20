bbd6b04d04d97ed1e0bdb072bb28a7f3c93a6ce6
gurkanmustafa0 <gurkanmustafa0@gmail.com>
2026-09-20 19:09:51 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const V='m3';
+self.addEventListener('install',e=>e.waitUntil(caches.open(V).then(c=>c.addAll(['./','index.html','manifest.webmanifest','icon-180.png','icon-512.png']))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!=V).map(x=>caches.delete(x))))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
